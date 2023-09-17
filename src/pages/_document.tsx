4efdb087cddbd9ef2be939d19f427e1c28a324e6
Tân Phạm <tanpham1104@gmail.com>
2023-09-17 16:26:59 +0700

    extract antd to static css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import genAntdCss from '@/scripts/genAntdCss';
+import { StyleProvider, createCache } from '@ant-design/cssinjs';
 
-export default function Document() {
+const MyDocument = () => {
   return (
     <Html lang="en">
       <Head />
@@ -9,5 +11,36 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const cache = createCache();
+  let fileName = '';
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => (
+        <StyleProvider cache={cache}>
+          <App {...props} />
+        </StyleProvider>
+      ),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  fileName = genAntdCss({
+    cache,
+  });
+
+  return {
+    ...initialProps,
+    head: [
+      <>
+        {fileName && <link rel="stylesheet" href={`/${fileName}`} />}
+        {initialProps.head}
+      </>,
+    ],
+  };
+};
+
+export default MyDocument;
