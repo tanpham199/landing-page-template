5d4306865266ad85590625ba5a37e0cf7ecd2306
Tân Phạm <tanpham1104@gmail.com>
2023-09-18 23:42:30 +0700

    responsive header

--- src/layouts/HomeLayout/index.tsx
+++ src/layouts/HomeLayout/index.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from 'react';
-import { Layout, FloatButton, theme, Button, Space } from 'antd';
+import { Layout, FloatButton, theme, Button, Space, Dropdown } from 'antd';
+import { UnorderedListOutlined } from '@ant-design/icons';
 import Image from 'next/image';
 import Link from 'next/link';
 import { APP_LOGO } from '@/constants';
@@ -38,7 +39,7 @@ const HomeLayout = ({ children }: PropsWithChildren) => {
         <Link href={PagePath.Home} className={classNames(styles.logo, 'center')}>
           <Image src={APP_LOGO} alt="Logo" priority />
         </Link>
-        <Space size="large">
+        <Space size="large" className={styles.nav}>
           <ul className={styles.menu}>
             {NAV_ITEMS.map(({ label, path }) => (
               <li key={label}>
@@ -60,6 +61,24 @@ const HomeLayout = ({ children }: PropsWithChildren) => {
             </Button>
           </Link>
         </Space>
+        <Dropdown
+          placement="bottomRight"
+          overlayClassName="w-32"
+          menu={{
+            items: NAV_ITEMS.map(({ label, path }) => ({
+              key: path as string,
+              label: <Link href={path}>{label}</Link>,
+            })).concat({
+              key: 'contact',
+              label: <Link href={PagePath.Contact}>Contact Us</Link>,
+            }),
+            theme: 'dark',
+          }}
+        >
+          <button type="button" className={styles.hamburger}>
+            <UnorderedListOutlined className="text-white text-2xl" />
+          </button>
+        </Dropdown>
       </Header>
       <Content>{children}</Content>
       <Footer className="text-center">A2A SOFTWARE ©2023</Footer>
